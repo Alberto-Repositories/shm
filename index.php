702383ea0e98aa612c423bd937ec6fb1064968ed
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-02-26 20:20:29 -0600

    cambios en las imagenes

--- index.php
+++ index.php
@@ -370,26 +370,26 @@
                     <p>Contamos con una variedad de productos.</p>
                 </div>
 
-                <!-- <ul id="portfolio-flters" class="d-flex justify-content-center">
+                <ul id="portfolio-flters" class="d-flex justify-content-center">
                     <li data-filter="*" class="filter-active">Todo</li>
-                    <li data-filter=".filter-app">App</li>
-                    <li data-filter=".filter-card">Card</li>
-                    <li data-filter=".filter-web">Web</li>
-                </ul> -->
+                    <li data-filter=".filter-app">Alcoholero</li>
+                    <li data-filter=".filter-card">Garrafas</li>
+                    <li data-filter=".filter-web">Maquinas</li>
+                </ul>
 
-                <!-- <div class="row portfolio-container">
+                <div class="row portfolio-container">
 
                     <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="portfolio-img"><img src="assets/img/alcoholero/alcoholera1-2(medio)R-2835g.png" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
-                            <h4>App 1</h4>
-                            <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                            <!-- <h4>App 1</h4> -->
+                            <p>Alcoholera 1-2 (medio) R-28 35g</p>
+                            <a href="assets/img/alcoholero/alcoholera1-2(medio)R-2835g.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Alcoholera 1-2 (medio) R-28 35g"><i class="bx bx-plus"></i></a>
+                            <!-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> -->
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Web 3</h4>
@@ -397,19 +397,19 @@
                             <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt=""></div>
+                        <div class="portfolio-img"><img src="assets/img/alcoholero/alcoholero-1-8-(un octavo)-R-28-15g.png" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
-                            <h4>App 2</h4>
-                            <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                            <!-- <h4>App 2</h4> -->
+                            <p>Alcoholero 1-8 (un octavo) R-28 15g</p>
+                            <a href="assets/img/alcoholero/alcoholero-1-8-(un octavo)-R-28-15g.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Alcoholero 1-8 (un octavo) R-28 15g"><i class="bx bx-plus"></i></a>
+                            <!-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> -->
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Card 2</h4>
@@ -417,9 +417,9 @@
                             <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Web 2</h4>
@@ -427,19 +427,28 @@
                             <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt=""></div>
+                        <div class="portfolio-img"><img src="assets/img/alcoholero/alcoholero-1L-R-28-55g.png" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
-                            <h4>App 3</h4>
-                            <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                            <!-- <h4>App 3</h4> -->
+                            <p>Alcoholero 1L R 28 20g</p>
+                            <a href="assets/img/alcoholero/alcoholero-1L-R-28-55g.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Alcoholero 1L R 28 20g"><i class="bx bx-plus"></i></a>
+                            <!-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-img"><img src="assets/img/alcoholero/alcoholero1-4(uncuarto)R-28-20g-.png" class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <!-- <h4>App 3</h4> -->
+                            <p>Alcoholero (un cuarto) R 28 20g</p>
+                            <a href="assets/img/alcoholero/alcoholero1-4(uncuarto)R-28-20g-.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Alcoholero (un cuarto) R 28 20g"><i class="bx bx-plus"></i></a>
+                            <!-- <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a> -->
                         </div>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Card 1</h4>
@@ -447,9 +456,9 @@
                             <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Card 3</h4>
@@ -457,9 +466,9 @@
                             <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt=""></div>
                         <div class="portfolio-info">
                             <h4>Web 3</h4>
@@ -467,9 +476,9 @@
                             <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                         </div>
-                    </div>
+                    </div> -->
 
-                </div> -->
+                </div>
 
             </div>
         </section>
